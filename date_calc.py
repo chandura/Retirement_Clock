08fa81dce924204e41addc415ba894cd5daa01d3
Mark Fox-Smith <mark@fox-smith.co.uk>
2018-02-24 19:47:52 +0000

    Initial code base
    Opening code base for the retirement calculator

---
+++ date_calc.py
@@ -0,0 +1,89 @@
+import datetime
+from dateutil.relativedelta import relativedelta
+
+define_optimistic = 55
+define_pragmatic = 60
+define_pessimistic = 65
+
+class Retirement:
+	def __init__ (self, dob, progress):
+		self.dob = dob
+		self.progress = progress
+
+	@property
+	def lifetime(self):
+		return abs((self.progress - self.dob).days)
+
+	@property
+	def optimistic(self):
+		return self.dob + relativedelta(years=+define_optimistic)
+
+	@property
+	def optimistic_days(self):
+		return abs((self.dob + relativedelta(years=+define_optimistic) - self.progress).days)
+
+	@property
+	def optimistic_months(self):
+		target_date = self.dob + relativedelta(years=+define_optimistic)
+		months = (target_date.year - self.progress.year) * 12 + (target_date.month - self.progress.month)
+		return months
+
+	@property
+	def optimistic_years(self):
+		target_date = self.dob + relativedelta(years=+define_optimistic)
+		years = (target_date.year - self.progress.year)
+		return years
+
+	@property
+	def pragmatic(self):
+		return self.dob + relativedelta(years=+define_pragmatic)
+
+	@property
+	def pragmatic_days(self):
+		return abs((self.dob + relativedelta(years=+define_pragmatic) - self.progress).days)
+
+
+	@property
+	def pragmatic_months(self):
+		target_date = self.dob + relativedelta(years=+define_pragmatic)
+		months = (target_date.year - self.progress.year) * 12 + (target_date.month - self.progress.month)
+		return months	
+
+	@property
+	def pragmatic_years(self):
+		target_date = self.dob + relativedelta(years=+define_pragmatic)
+		years = (target_date.year - self.progress.year)
+		return years
+
+	@property
+	def pessimistic(self):
+		return self.dob + relativedelta(years=+define_pessimistic)
+
+	@property
+	def pessimistic_days(self):
+		return abs((self.dob + relativedelta(years=+define_pessimistic) - self.progress).days)
+
+	@property
+	def pessimistic_months(self):
+		target_date = self.dob + relativedelta(years=+define_pessimistic)
+		months = (target_date.year - self.progress.year) * 12 + (target_date.month - self.progress.month)
+		return months	
+
+	@property
+	def pessimistic_years(self):
+		target_date = self.dob + relativedelta(years=+define_pessimistic)
+		years = (target_date.year - self.progress.year)
+		return years
+
+	@property
+	def optimistic_percent(self):
+		return float((self.dob + relativedelta(years=+define_optimistic) - self.progress).days) / float((self.lifetime + (self.dob + relativedelta(years=+define_optimistic) - self.progress).days)) * 100
+
+
+	@property
+	def pragmatic_percent(self):
+		return float((self.dob + relativedelta(years=+define_pragmatic) - self.progress).days) / float((self.lifetime + (self.dob + relativedelta(years=+define_pragmatic) - self.progress).days)) * 100
+
+	@property
+	def pessimistic_percent(self):
+		return float((self.dob + relativedelta(years=+define_pessimistic) - self.progress).days) / float((self.lifetime + (self.dob + relativedelta(years=+define_pessimistic) - self.progress).days)) * 100		
---
+++ test_dob.py
@@ -0,0 +1,31 @@
+import unittest
+import datetime
+from date_calc import Retirement
+
+class TestBetween(unittest.TestCase):
+
+	def test_days(self):
+		days = Retirement(datetime.date(1967, 6, 2), datetime.date(1968, 6, 2))
+
+		self.assertEqual(days.lifetime, 366)
+		
+	def test_optimisitc(self):
+		optimistic = Retirement(datetime.date(1967, 6, 2), datetime.date(2019, 6, 2))
+
+		self.assertEqual(str(optimistic.optimistic), '2022-06-02')
+		self.assertEqual(optimistic.optimistic_days, 1096)
+
+	def test_pragmatic(self):
+		pragmatic = Retirement(datetime.date(1967, 6, 2), datetime.date(2019, 6, 2))
+
+		self.assertEqual(str(pragmatic.pragmatic), '2027-06-02')
+		self.assertEqual(pragmatic.pragmatic_days, 2922)
+
+	def test_pessimistic(self):
+		pessimistic = Retirement(datetime.date(1967, 6, 2), datetime.date(2019, 6, 2))
+
+		self.assertEqual(str(pessimistic.pessimistic), '2032-06-02')
+		self.assertEqual(pessimistic.pessimistic_days, 4749)
+
+if __name__ == '__main__':
+	unittest.main()
